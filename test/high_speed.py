28dee88d81035b5347d3330c27f19d36755661ca
Manya <Lenaio@yandex.ru>
2020-06-11 18:27:25 +0300

    Добавила конфтест <3

--- test/high_speed.py
+++ test/high_speed.py
@@ -1,13 +1,4 @@
 # -*- coding: utf-8 -*-
-from selenium.common.exceptions import NoSuchElementException
-import pytest
-from fixture.application import Application
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_high_speed(app):
     app.open_home_page()
