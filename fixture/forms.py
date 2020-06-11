ca9cf8d51d3099c124e02a2e6dfa2e17b91354d1
Manya <Lenaio@yandex.ru>
2020-06-11 18:44:25 +0300

    Добавила тест 1.11. Надо доработать метод заказа дня в фильтре.

--- fixture/forms.py
+++ fixture/forms.py
@@ -5,12 +5,33 @@ class FormsHelper:
     def __init__(self, app):
         self.app = app
 
+    def select_extra_forms(self):
+        wd = self.app.wd
+        wd.find_element_by_xpath("(//div[text()='III. Дополнительные формы'])").click()
+        time.sleep(3)
+
+    def select_operational_reports(self):
+        wd = self.app.wd
+        wd.find_element_by_xpath("(//div[text()='I. Оперативная отчетность'])").click()
+        time.sleep(3)
+
+    def select_analytical_forms(self):
+        wd = self.app.wd
+        wd.find_element_by_xpath("(//div[text()='II. Аналитические отчеты и справки'])").click()
+        time.sleep(3)
+
+    def select_TN(self):
+        wd = self.app.wd
+        wd.find_element_by_xpath("(//div[text()='IV. Технологические нарушения'])").click()
+        time.sleep(3)
+
+
     def select_high_speed(self):
         wd = self.app.wd
         wd.find_element_by_xpath("//span[text()='Скоростной ход']").click()
         time.sleep(15)
 
-    def select_extra_forms(self):
+    def select_1_11(self):
         wd = self.app.wd
-        wd.find_element_by_xpath("(//div[text()='III. Дополнительные формы'])").click()
-        time.sleep(3)
+        wd.find_element_by_xpath("//span[text()='1.11 Эксплуатационные последствия']").click()
+        time.sleep(15)
