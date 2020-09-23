47d32b51dca927ce0c4d542de94db2c1809e6880
cherepanova <busbunny.k38@gmail.com>
2020-09-23 17:24:49 +0300

    for jenkins

--- test/test_1_1.py
+++ test/test_1_1.py
@@ -10,21 +10,21 @@
 #     app.filter.click_filter_ok()
 #     app.details.detail_total()
 
-# def test_detail_all(app):
-#     app.open_home_page()
-#     app.click_on_clear()
-#     app.forms.select_analytical_forms()
-#     app.forms.select_1_1()
-#     app.filter.open_filter()
-#     app.filter.filter_for_the_current_year()
-#     app.filter.click_filter_ok()
-#     app.details.detail_all()
-
-def test_filter_counter(app):
+def test_detail_all(app):
     app.open_home_page()
     app.click_on_clear()
     app.forms.select_analytical_forms()
     app.forms.select_1_1()
-    app.counter.counter_coulm3_last_row(app)
+    app.filter.open_filter()
+    app.filter.filter_for_the_current_year()
+    app.filter.click_filter_ok()
+    app.details.detail_all()
+
+# def test_filter_counter(app):
+#     app.open_home_page()
+#     app.click_on_clear()
+#     app.forms.select_analytical_forms()
+#     app.forms.select_1_1()
+#     app.counter.counter_coulm3_last_row(app)
 
 
