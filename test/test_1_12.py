a62269fa101d9a002e52086c01dc1f89ab3c48d2
cherepanova <busbunny.k38@gmail.com>
2020-09-29 14:57:17 +0300

    for jenkins

--- test/test_1_12.py
+++ test/test_1_12.py
@@ -17,12 +17,12 @@ def test_detail_total(app):
    app.filter.click_filter_ok()
    app.details.detail_total()
 
-# def test_detail_all(app):
-#     app.open_home_page()
-#     app.click_starting_overlay()
-#     app.forms.select_analytical_forms()
-#     app.forms.select_1_12()
-#     app.filter.open_filter()
-#     app.filter.filter_for_the_current_year()
-#     app.filter.click_filter_ok()
-#     app.details.detail_all()
+def test_detail_all(app):
+    app.open_home_page()
+    app.click_starting_overlay()
+    app.forms.select_analytical_forms()
+    app.forms.select_1_12()
+    app.filter.open_filter()
+    app.filter.filter_for_the_current_year()
+    app.filter.click_filter_ok()
+    app.details.detail_all()
--- test/test_3_1.py
+++ test/test_3_1.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 import time
 
-# def test_detail_all(app):
-#     app.open_home_page()
-#     app.click_starting_overlay()
-#     app.forms.select_analytical_forms()
-#     app.forms.select_3_1()
-#     app.filter.open_filter()
-#     app.filter.filter_for_2019()
-#     app.filter.click_filter_ok()
-#     app.details.detail_all()
+def test_detail_all(app):
+    app.open_home_page()
+    app.click_starting_overlay()
+    app.forms.select_analytical_forms()
+    app.forms.select_3_1()
+    app.filter.open_filter()
+    app.filter.filter_for_2019()
+    app.filter.click_filter_ok()
+    app.details.detail_all()
 
-# def test_filter_counter(app):
-#     app.open_home_page()
-#     app.click_starting_overlay()
-#     app.forms.select_analytical_forms()
-#     app.forms.select_3_1()
-#     time.sleep(5)
-#     app.counter.counter_coulm9_first_row(app)
+def test_filter_counter(app):
+    app.open_home_page()
+    app.click_starting_overlay()
+    app.forms.select_analytical_forms()
+    app.forms.select_3_1()
+    time.sleep(5)
+    app.counter.counter_coulm9_first_row(app)
 
 def test_with_2_1(app):
     app.open_home_page()
