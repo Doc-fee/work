502f778b2563f5f887f0ebfa13e10ced9e2261d8
cherepanova <busbunny.k38@gmail.com>
2020-10-30 16:36:05 +0300

    for home

--- fixture/sum_table.py
+++ fixture/sum_table.py
@@ -7,19 +7,14 @@ class SumTableHelper:
 
     def sum_rows(self, i):
         wd = self.app.wd
-        # row_count = len(wd.find_elements_by_xpath("//div[@id='contentScroll']//tbody[@id='dataBody']/tr"))
-        #
-        # total_number = wd.find_element_by_xpath("//tbody[@id='dataBody']/tr[%s]/td[3]" % row_count).text
-
         coulm_count = len(wd.find_elements_by_xpath("//div[@id='contentScroll']//tbody[@id='dataBody']/tr[1]/td"))
         rows = wd.find_elements_by_xpath("//div[@id='contentScroll']//tbody[@id='dataBody']/tr")
-        for i in range(0, coulm_count):
+        for i in range(1, coulm_count):
             sum = 0
-            if i != 0 and i % 3 != 0:
+            if i != 1 and i % 3 != 0:
                 for row in rows:
-                    r = row.find_element_by_xpath("//td[%s]" %i).text
-                    i = i+1
-                    # if r == '':
-                    #     r = 0
-                    # int_r = int(r)
-                    # sum = sum + int_r
+                    r = row.find_element_by_xpath("//td[%s]/a" %i).text
+                    if r == '':
+                        r = 0
+                    int_r = int(r)
+                    sum = sum + int_r
